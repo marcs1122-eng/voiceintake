920172d668e26e9f052a07cd21a18ed28a640826
marcs1122-eng <marcs1122@gmail.com>
2026-03-04 17:13:42 -0500

    Change model_id from 'eleven_turbo_v2' to 'eleven_flash_v2_5'

--- app/api/audio/route.js
+++ app/api/audio/route.js
@@ -22,7 +22,7 @@ export async function GET(request) {
         },
         body: JSON.stringify({
           text: text,
-          model_id: 'eleven_turbo_v2',
+          model_id: 'eleven_flash_v2_5',
           voice_settings: {
             stability: 0.5,
             similarity_boost: 0.75,
